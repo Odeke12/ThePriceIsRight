fdac57f063f6a244af640af30580a43136e04648
Odeke12 <trevodex@gmail.com>
2022-08-28 22:49:59 +0300

    Updated AttacherViews.js

--- Views/AttacherViews.js
+++ Views/AttacherViews.js
@@ -1,83 +1,93 @@
-import React from 'react';
-import PlayerViews from './PlayerViews';
+import React from "react";
+import PlayerViews from "./PlayerViews";
+import { Button } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const exports = { ...PlayerViews };
 
 exports.Wrapper = class extends React.Component {
-    render() {
-        const { content } = this.props;
-        return (
-            <div className="Attacher">
-                <h2>Attacher (Trevor)</h2>
-                {content}
-            </div>
-        );
-    }
-}
+  render() {
+    const { content } = this.props;
+    return (
+      <div className="Attacher">
+        <h2>Attacher (Pauline)</h2>
+        {content}
+      </div>
+    );
+  }
+};
 
 exports.Attach = class extends React.Component {
-    render() {
-        const { parent } = this.props;
-        const { ctcInfoStr } = this.state || {};
-        return (
-            <div>
-                Please paste the contract info to attach to:
-                <br />
-                <textarea spellCheck="false"
-                    className='ContractInfo'
-                    onChange={(e) => this.setState({ ctcInfoStr: e.currentTarget.value })}
-                    placeholder='{}'
-                />
-                <br />
-                <button
-                    disabled={!ctcInfoStr}
-                    onClick={() => parent.attach(ctcInfoStr)}
-                >Attach</button>
-            </div>
-        );
-    }
-}
+  render() {
+    const { parent } = this.props;
+    const { ctcInfoStr } = this.state || {};
+    return (
+      <div>
+        Please paste the contract info to attach to:
+        <br />
+        <textarea
+          spellCheck="false"
+          className="ContractInfo"
+          onChange={(e) => this.setState({ ctcInfoStr: e.currentTarget.value })}
+          placeholder="{}"
+        />
+        <br />
+        <Button
+          variant="contained"
+          disabled={!ctcInfoStr}
+          onClick={() => parent.attach(ctcInfoStr)}
+        >
+          Attach
+        </Button>
+      </div>
+    );
+  }
+};
 
 exports.Attaching = class extends React.Component {
-    render() {
-        return (
-            <div>
-                Attaching, please wait...
-            </div>
-        );
-    }
-}
+  render() {
+    return (
+      <div>
+        Attaching, please wait <CircularProgress />
+      </div>
+    );
+  }
+};
 
 exports.AcceptTerms = class extends React.Component {
-    render() {
-        const { wager, standardUnit, parent } = this.props;
-        const { disabled } = this.state || {};
-        return (
-            <div>
-                The terms of the game are:
-                <br /> Wager: {wager} {standardUnit}
-                <br />
-                <button
-                    disabled={disabled}
-                    onClick={() => {
-                        this.setState({ disabled: true });
-                        parent.termsAccepted();
-                    }}
-                >Accept terms and pay wager</button>
-            </div>
-        );
-    }
-}
+  render() {
+    const { wager, standardUnit, parent } = this.props;
+    const { disabled } = this.state || {};
+    return (
+      <div>
+        The terms of the game are:
+        <br /> Wager: {wager} {standardUnit}
+        <br />
+        <Button
+          variant="contained"
+          disabled={disabled}
+          onClick={() => {
+            this.setState({ disabled: true });
+            parent.termsAccepted();
+          }}
+        >
+          Accept terms and pay wager
+        </Button>
+      </div>
+    );
+  }
+};
 
 exports.WaitingForTurn = class extends React.Component {
-    render() {
-        return (
-            <div>
-                Waiting for the other player...
-                <br />Think about which move you want to play.
-            </div>
-        );
-    }
-}
+  render() {
+    return (
+      <div>
+        Waiting for the other player <CircularProgress />
+        <br />
+        Think about the value you are going to guess
+      </div>
+    );
+  }
+};
 
 export default exports;
